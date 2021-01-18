e635850c163b38124a9bbf02636c59a5e2815a2d
poojachilukuri <pooja.ramlingam@gmail.com>
2021-01-18 11:57:21 -0500

    game-project

---
+++ src/app/gamecontrol/gamecontrol.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit,EventEmitter, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-gamecontrol',
+  templateUrl: './gamecontrol.component.html',
+  styleUrls: ['./gamecontrol.component.css']
+})
+export class GamecontrolComponent implements OnInit {
+  @Output() intervalFired= new EventEmitter<number>();
+  interval;
+  lastNumber=0;
+  constructor() { }
+
+  ngOnInit(): void {
+  } 
+  onStartGame(){
+    this.interval=setInterval(()=>{
+      this.intervalFired.emit(this.lastNumber + 1);
+      this.lastNumber++;
+    },
+      1000);
+  }
+  onEndGame(){
+    clearInterval(this.interval);
+  }
+
+}
